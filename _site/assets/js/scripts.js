de9d29bd22afa44ee85592c874a781aaf142dfa9
Anubhav <anubhav.roy.05@protonmail.com>
2025-10-05 01:13:24 +0530

    fix

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -52,7 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
 // --------------------------
 const blurbs = [
   "/2025/09/13/my-first-post.html",
-  "/2025/09/16/morality.md"
+  "/2025/09/16/morality.html",
+  "/2025/10/05/dnd-with-deepseek.html",
+  "/2025/09/23/ai-aversion.html"
   // Add more blurb URLs here as needed
 ];
 
